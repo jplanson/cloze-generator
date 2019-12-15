281f27a227c7a459cc56e4cda299a0b06ad19f04
jplanson <jlanson7026@gmail.com>
2019-12-14 22:19:07 -0500

    Initial commit for ClozeGenerator

---
+++ src/com/jplanson/cloze/main/ClozeGeneratorFrame.java
@@ -0,0 +1,10 @@
+package com.jplanson.cloze.main;
+
+import javax.swing.JFrame;
+
+public class ClozeGeneratorFrame extends JFrame 
+{
+
+	private static final long serialVersionUID = 8826062099639243348L;
+
+}
